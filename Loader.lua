e112866daab7fed764de8357531bb69af068610f
Runs111 <67319227+Runs111@users.noreply.github.com>
2023-04-13 16:57:28 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,10 @@
+local games = {
+    [{6839171747}] = "k",
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
