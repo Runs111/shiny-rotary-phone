9bf35912e8c1e66cc590f80d00c0aea460a07e3a
Shiba <67319227+Runs111@users.noreply.github.com>
2023-04-14 15:54:29 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local games = {
-    [{6839171747}] = "k",
+    [{6839171747}] = "https://raw.githubusercontent.com/Runs111/shiny-rotary-phone/main/Doors/Loader.lua",
 }
 
 for ids, url in next, games do
